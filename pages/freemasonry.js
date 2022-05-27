81827027bbb8746fe53dccf778adca972b17f740
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-05-27 09:16:01 -0400

    added pages

---
+++ pages/freemasonry.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const freemasonry = () => {
+  return <div>freemasonry</div>;
+};
+
+export default freemasonry;
---
+++ pages/windsor-lodge.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const windsorlodge = () => {
+  return <div>windsor-lodge</div>;
+};
+
+export default windsor - lodge;
