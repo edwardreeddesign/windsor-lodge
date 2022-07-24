3165890d6ec8d72de5a052db2d8d080d8783d705
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-07-24 15:16:09 -0400

    added new date on banner

--- components/nav/Banner.js
+++ components/nav/Banner.js
@@ -1,10 +1,10 @@
-import { StyledBanner } from "./NavStyles";
+import { StyledBanner } from './NavStyles';
 
 const Banner = () => {
   return (
     <StyledBanner>
       <p>* Night of Comradery *</p>
-      <p>Friday, July 8th, 2022 - 7:00 p.m.</p>
+      <p>Friday, July 29th, 2022 - 7:00 p.m.</p>
     </StyledBanner>
   );
 };
