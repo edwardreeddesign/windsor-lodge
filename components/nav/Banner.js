ec054d11dea8c1db51e18ecf730a08be4c4fe4b5
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-09-23 07:17:17 -0400

    changed banner dates

--- components/nav/Banner.js
+++ components/nav/Banner.js
@@ -3,10 +3,6 @@ import { StyledBanner } from './NavStyles';
 const Banner = () => {
   return (
     <StyledBanner>
-      <p>* Past Master and Officers Meeting*</p>
-      <p>
-        Saturday August 20th, 2022 at 11am at the Masonic Temple top floor.{' '}
-      </p>
       <p>* Regular Business Meeting *</p>
       <p>Friday September 9th, 2022</p>
       <p>* 3rd Degree *</p>
