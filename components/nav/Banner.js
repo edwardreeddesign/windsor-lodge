0356790bb5cb47f3e23bac9fa70c56435bcd3db0
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-10-27 09:58:04 -0400

    changed banner dates

--- components/nav/Banner.js
+++ components/nav/Banner.js
@@ -4,9 +4,7 @@ const Banner = () => {
   return (
     <StyledBanner>
       <p>* Regular Business Meeting *</p>
-      <p>Friday, October 14, 2022 – 7:30 p.m.</p>
-      <p>* 1st Degree *</p>
-      <p>Friday, October 21, 2022 – 7:30 p.m.</p>
+      <p>Friday, November 4, 2022 – 7:30 p.m.</p>
     </StyledBanner>
   );
 };
