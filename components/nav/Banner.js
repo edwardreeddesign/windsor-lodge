37bad864bd2d781dd41c1c8199a3e2fd394ce656
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-11-23 06:46:10 -0500

    changed time on banner

--- components/nav/Banner.js
+++ components/nav/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
       <p>
         * Great Western Lodge 47 Installation * December 1st Start at 7:30 p.m.
       </p>
-      <p>* Harmony Lodge 579 Installation * December 3rd Start at 2:30 p.m. </p>
+      <p>* Harmony Lodge 579 Installation * December 3rd Start at 2:00 p.m. </p>
     </StyledBanner>
   );
 };
