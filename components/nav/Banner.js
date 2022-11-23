3e2307ce2bb9a89a8bc73d1b5ee4ef224c2cab62
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-11-22 19:45:00 -0500

    added dates to nav banner

--- components/nav/Banner.js
+++ components/nav/Banner.js
@@ -5,8 +5,10 @@ const Banner = () => {
     <StyledBanner>
       <p>* Regular Business Meeting - Election of Officers *</p>
       <p>Friday, December 2nd, 2022 – 7:30 p.m.</p>
-      <p>* Great Western Lodge 47 Installation * Start at 7:30 p.m.</p>
-      <p>* Harmony Lodge 579 Installation * Start at 2:30 p.m. </p>
+      <p>
+        * Great Western Lodge 47 Installation * December 1st Start at 7:30 p.m.
+      </p>
+      <p>* Harmony Lodge 579 Installation * December 3rd Start at 2:30 p.m. </p>
     </StyledBanner>
   );
 };
