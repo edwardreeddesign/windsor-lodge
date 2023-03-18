0d7b980bef24e55862ec86df048c85e4f37c3862
Ed Reed <edreed@Eds-MacBook-Air.local>
2023-03-18 13:22:08 -0400

    changed banner

--- components/nav/Banner.js
+++ components/nav/Banner.js
@@ -3,7 +3,7 @@ import { StyledBanner } from './NavStyles';
 const Banner = () => {
   return (
     <StyledBanner>
-      <p>* Regular Meeting Friday, April 7th – 7:30 p.m. *</p>
+      <p>* Regular Meeting Friday, April 14th – 7:30 p.m. *</p>
       <p>* Dinner 6:30 p.m. *</p>
     </StyledBanner>
   );
